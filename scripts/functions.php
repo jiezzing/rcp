d7687b10825bb37bea6c3b42114b0d865637a47d
trd-admin <trd-admin@trd-admin.routeree98b0.com>
2019-11-06 16:58:05 +0800

    capital first letter of amount2words

--- scripts/functions.php
+++ scripts/functions.php
@@ -76,12 +76,13 @@
               }
             }
         }
+          var word = currencyToWords(sum).substr(0, 1).toUpperCase() + "" + currencyToWords(sum).substr(1);
           $("#total_amount").val(currencyWithCommas(sum));
           if(sum == 1){
-            $("#amount-in-words").val(currencyToWords(sum) + " Peso Only");
+            $("#amount-in-words").val(word + " peso only");
           }
           else{
-            $("#amount-in-words").val(currencyToWords(sum) + " Pesos Only");
+            $("#amount-in-words").val(word + " pesos only");
           }
       });
 
@@ -95,73 +96,68 @@
   }
 
   function currencyToWords(s) {
-    var th = ['', 'Thousand', 'Million', 'Billion', 'Trillion'];
-    var dg = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-    var tn = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
-    var tw = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+    var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+    var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     s = s.toString();
     s = s.replace(/[\, ]/g, '');
     if (s != parseFloat(s)) return 'not a number';
-    var x = s.indexOf('.');
-    var fulllength=s.length;
-    
-      if (x == -1) x = s.length;
-      if (x > 15) return 'too big';
-      var startpos=fulllength-(fulllength-x-1);
-      var n = s.split('');
-      var str = '';
-      var str1 = ''; // I added another word called cent
-      var sk = 0;
-      for (var i = 0; i < x; i++) {
-          if ((x - i) % 3 == 2) {
-              if (n[i] == '1') {
-                  str += tn[Number(n[i + 1])] + ' ';
-                  i++;
-                  sk = 1;
-              } else if (n[i] != 0) {
-                  str += tw[n[i] - 2] + ' ';
-                  sk = 1;
-              }
-          } else if (n[i] != 0) {
-              str += dg[n[i]] + ' ';
-              if ((x - i) % 3 == 0) str += 'Hundred ';
-              sk = 1;
-          }
-          if ((x - i) % 3 == 1) {
-              if (sk) str += th[(x - i - 1) / 3] + ' ';
-              sk = 0;
-          }
-      }
-      if (x != s.length) {
-          str += 'And '; //i change the word point to and 
-          str1 += 'Centavos '; //i added another word called cent
-          //for (var i = x + 1; i < y; i++) str += dg[n[i]] + ' ' ;
-      var j=startpos;
+      var x = s.indexOf('.');
+      var fulllength=s.length;
       
-      for (var i = j; i < fulllength; i++) {
-      
-          if ((fulllength - i) % 3 == 2) {
-              if (n[i] == '1') {
-                  str += tn[Number(n[i + 1])] + ' ';
-                  i++;
-                  sk = 1;
-              } else if (n[i] != 0) {
-                  str += tw[n[i] - 2] + ' ';
-          
-                  sk = 1;
-              }
-          } else if (n[i] != 0) {
-      
-              str += dg[n[i]] + ' ';
-              if ((fulllength - i) % 3 == 0) str += 'Hundred ';
-              sk = 1;
-          }
-          if ((fulllength - i) % 3 == 1) {
-      
-              if (sk) str += th[(fulllength - i - 1) / 3] + ' ';
-              sk = 0;
-          }
-      }
+        if (x == -1) x = s.length;
+        if (x > 15) return 'too big';
+        var startpos=fulllength-(fulllength-x-1);
+        var n = s.split('');
+        var str = '';
+        var str1 = ''; // I added another word called cent
+        var sk = 0;
+        for (var i = 0; i < x; i++) {
+            if ((x - i) % 3 == 2) {
+                if (n[i] == '1') {
+                    str += tn[Number(n[i + 1])] + ' ';
+                    i++;
+                    sk = 1;
+                } else if (n[i] != 0) {
+                    str += tw[n[i] - 2] + ' ';
+                    sk = 1;
+                }
+            } else if (n[i] != 0) {
+                str += dg[n[i]] + ' ';
+                if ((x - i) % 3 == 0) str += 'hundred ';
+                sk = 1;
+            }
+            if ((x - i) % 3 == 1) {
+                if (sk) str += th[(x - i - 1) / 3] + ' ';
+                sk = 0;
+            }
+        }
+        if (x != s.length) {
+            str += 'And '; //i change the word point to and 
+            str1 += 'Centavos '; //i added another word called cent
+            //for (var i = x + 1; i < y; i++) str += dg[n[i]] + ' ' ;
+        var j=startpos;
+        for (var i = j; i < fulllength; i++) {
+            if ((fulllength - i) % 3 == 2) {
+                if (n[i] == '1') {
+                    str += tn[Number(n[i + 1])] + ' ';
+                    i++;
+                    sk = 1;
+                } else if (n[i] != 0) {
+                    str += tw[n[i] - 2] + ' ';
+                    sk = 1;
+                }
+            } else if (n[i] != 0) {
+                str += dg[n[i]] + ' ';
+                if ((fulllength - i) % 3 == 0) str += 'hundred ';
+                sk = 1;
+            }
+            if ((fulllength - i) % 3 == 1) {
+                if (sk) str += th[(fulllength - i - 1) / 3] + ' ';
+                sk = 0;
+            }
+        }
       }
     var result=str.replace(/\s+/g, ' ') + str1;
       //return str.replace(/\s+/g, ' ');
