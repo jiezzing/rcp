7307333f9d6b0785fc0a4ed2fef2174e780899f5
trd-admin <trd-admin@trd-admin.routeree98b0.com>
2019-11-12 19:00:00 +0800

    bootstrap.min.css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/klorofil/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/main.css">
 	<link rel="stylesheet" href="assets/vendor/fonts/google-font.css">
 	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
